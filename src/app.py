1b0d6a52a09a803c1fff6c77ef4457d35bfca94d
Max <max.white.rabbit@gmail.com>
2020-03-14 23:31:26 +0500

    try it

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ def validate_post_data(data: dict) -> bool:
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hell O!'
+    return 'Hell world'
 
 
 @app.route('/api', methods=['GET', 'POST'])
